7a605a96f95d4bef36890c510620bfc2d5851cdf
Joseph Greening <45278068+YoloPigeon@users.noreply.github.com>
2019-03-20 16:42:42 +1300

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -4,7 +4,7 @@
 $(document).keypress(function(e) {
     var keycode = (e.keyCode ? e.keyCode : e.which);
     if (keycode === '13') {
-      validate();
+    document.getElementById("submit").click();
     }
 });
 
